851b4b0f07dbb76ebc7ac40074b417bbd8f7767b
Thatsaphon <thatphon123@hotmail.com>
2025-01-18 17:08:56 +0700

    draft home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className='w-full flex flex-col h-full pt-4 px-4'>
+      <section id='image-slider' className='flex justify-center w-full'>
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={'https://placehold.co/600x400/EEE/31343C'}
+          alt='placeholder'
+          width={600}
+          height={400}
+          unoptimized
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </section>
+      <section
+        id='company-profile'
+        className='grid lg:grid-cols-[2fr_1fr_1fr] md:grid-cols-[2fr_1fr] justify-center gap-2 mt-2 xl:grid-cols-[42rem_300px_300px]'
+      >
+        <div className='max-w-2xl row-span-2 justify-self-end col-span-2 md:col-span-1'>
+          <h2 className='text-2xl font-bold'>รู้จักกับเรา</h2>
+          <div className='pl-10'>
+            <h3 className='text-2xl font-bold relative'>
+              <div className='absolute -left-5 -top-1'>●</div>
+              2022
+            </h3>
+            <p className='relative'>
+              <div className='absolute -left-[14.5px] border-l-4 border-[var(--text-base)] -top-5 -bottom-3'></div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              voluptates at consectetur impedit sit voluptas cum ut vel ipsa
+              tempore repellat illum id natus omnis officiis, atque recusandae?
+              Molestias, iste.
+            </p>
+            <h3 className='text-2xl font-bold relative'>
+              <div className='absolute -left-5 -top-1'>●</div>
+              2023
+            </h3>
+            <p className='relative'>
+              <div className='absolute -left-[14.5px] border-l-4 border-[var(--text-base)] -top-5 -bottom-3'></div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              voluptates at consectetur impedit sit voluptas cum ut vel ipsa
+              tempore repellat illum id natus omnis officiis, atque recusandae?
+              Molestias, iste.
+            </p>
+            <h3 className='text-2xl font-bold relative'>
+              <div className='absolute -left-5 -top-1'>●</div>
+              2024
+            </h3>
+            <p className='relative'>
+              <div className='absolute -left-[14.5px] border-l-4 border-[var(--text-base)] -top-5 bottom-2'></div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              voluptates at consectetur impedit sit voluptas cum ut vel ipsa
+              tempore repellat illum id natus omnis officiis, atque recusandae?
+              Molestias, iste.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <Image
+          src={'https://placehold.co/300x200/EEE/31343C'}
+          alt='placeholder'
+          width={300}
+          height={200}
+          unoptimized
+          className='justify-self-center w-full max-h-[200px] xl:justify-self-start'
+        />
+        <Image
+          src={'https://placehold.co/300x200/EEE/31343C'}
+          alt='placeholder'
+          width={300}
+          height={200}
+          unoptimized
+          className='justify-self-center w-full max-h-[200px] xl:justify-self-start'
+        />
+        <Image
+          src={'https://placehold.co/300x200/EEE/31343C'}
+          alt='placeholder'
+          width={300}
+          height={200}
+          unoptimized
+          className='justify-self-center w-full max-h-[200px] xl:justify-self-start'
+        />
+        <Image
+          src={'https://placehold.co/300x200/EEE/31343C'}
+          alt='placeholder'
+          width={300}
+          height={200}
+          unoptimized
+          className='justify-self-center w-full max-h-[200px] xl:justify-self-start'
+        />
+      </section>
     </div>
-  );
+  )
 }
